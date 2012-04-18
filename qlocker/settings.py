ea649f63ab609c5baf7a6e5e11e2093036c5215d
John Williams <johnw@ubuntu.(none)>
2012-04-18 13:30:50 -0500

    settings

--- qlocker/settings.py
+++ qlocker/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'qlocker',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'cradle69',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
